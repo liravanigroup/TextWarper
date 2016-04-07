3cc4954ca3c6c0d9e1951f84c65be32c4f26c2f2
Sergej Povzaniuk <liravanigroup@gmail.com>
2016-04-07 16:14:46 +0300

    Added files via upload

--- model/TextContainer.java
+++ model/TextContainer.java
@@ -24,7 +24,10 @@ public class TextContainer {
 
         while (wordsFinder.hasMoreElements()) {
             String word = wordsFinder.nextToken();
-            if (word.length() > 3)
+            int minWordLong = 3;
+            if (isHaveSymbolOfStringEnd(word))
+                minWordLong = 4;
+            if (word.length() > minWordLong)
                 wordsBuffer.append(getOutWordWithMixedLetter(word)).append(" ");
             else
                 wordsBuffer.append(word).append(" ");
@@ -32,32 +35,42 @@ public class TextContainer {
         return wordsBuffer.toString();
     }
 
-    private String getOutWordWithMixedLetter(String wordToMix) {
-        char[] inWord = wordToMix.toCharArray();
-        return getImplodedOutWord(getCharArrayOfOutWordLetters(inWord, getNewCharIndexes(inWord)));
+    private boolean isHaveSymbolOfStringEnd(String word) {
+        return word.contains(",") || word.contains(".") || word.contains("!") || word.contains("?") || word.contains(":") || word.contains(";");
     }
 
-    private List<Integer> getNewCharIndexes(char[] inWord) {
+    private String getOutWordWithMixedLetter(String wordToMix) {
+        char[] inWord = wordToMix.toCharArray();
+        return getImplodedOutWord(getCharArrayOfOutWordLetters(inWord, getNewCharIndexes(inWord,wordToMix),wordToMix));
+    }
+
+    private List<Integer> getNewCharIndexes(char[] inWord, String wordToMix) {
         Random random = new Random();
         List<Integer> charIndexes = new ArrayList<>();
+        int workArea = inWord.length;
+        if(isHaveSymbolOfStringEnd(wordToMix))
+            --workArea;
         do {
-            int newIndex = random.nextInt(inWord.length - 1);
+            int newIndex = random.nextInt(workArea - 1);
             if (newIndex == 0)
                 continue;
             else if (!charIndexes.contains(newIndex))
                 charIndexes.add(newIndex);
-        } while (charIndexes.size() != inWord.length - 2);
+        } while (charIndexes.size() != workArea - 2);
+
         return charIndexes;
     }
 
-    private char[] getCharArrayOfOutWordLetters(char[] inWord, List<Integer> charIndexes) {
+    private char[] getCharArrayOfOutWordLetters(char[] inWord, List<Integer> charIndexes, String wordToMix) {
         char[] outWord = new char[inWord.length];
-        outWord[0] = inWord[0];
-        outWord[outWord.length - 1] = inWord[inWord.length - 1];
         int nextChar = 1;
         for (int charIndex : charIndexes) {
             outWord[nextChar++] = inWord[charIndex];
         }
+        outWord[0] = inWord[0];
+        outWord[outWord.length - 1] = inWord[inWord.length - 1];
+        if (isHaveSymbolOfStringEnd(wordToMix))
+            outWord[outWord.length - 2] = inWord[inWord.length - 2];
         return outWord;
     }
 
